338f558be189399137a795d30d672aae2bbe4600
David <david@kimmich.dev>
2026-01-27 15:10:53 +0100

    add slide in animation to valentine card and background image to body

--- src/components/valentine-calendar/valentine-calendar.tsx
+++ src/components/valentine-calendar/valentine-calendar.tsx
@@ -1,3 +1,4 @@
+import { motion } from "motion/react";
 import {
   ValentineCaldendarCard,
   type ValentineCaldendarCardData,
@@ -8,10 +9,26 @@ export interface ValentineCalendarProps {
 }
 
 export function ValentineCalendar({ data: cards }: ValentineCalendarProps) {
+  const initial = { opacity: 0, transform: "translateY(100%)" };
+  const whileInView = {
+    opacity: 1,
+    transform: "translateY(0%)",
+    transition: {
+      duration: 0.5,
+    },
+  };
+  const viewport = { margin: "250px", once: true };
+
   return (
     <div className="flex flex-col items-center gap-8 sm:flex-row sm:flex-wrap">
       {cards.map((card) => (
-        <ValentineCaldendarCard data={card} key={card.date.getTime()} />
+        <motion.div
+          initial={initial}
+          whileInView={whileInView}
+          viewport={viewport}
+        >
+          <ValentineCaldendarCard data={card} key={card.date.getTime()} />
+        </motion.div>
       ))}
     </div>
   );
